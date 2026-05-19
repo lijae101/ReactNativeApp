dc66cb68eea2041781bf42e6e0c3dc1d6956cdf1
Liam Janicke <liam.jaenicke@gmail.com>
2026-05-19 11:23:24 -0700

    Added Tab Navigation

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+import { styled } from "nativewind"
+
+
+const SafeAreaView = styled(RNSafeAreaView)
+
 
 const Settings = () => {
   return (
-    <View>
+    <SafeAreaView className="p-5 bg-background flex-1">
       <Text>Settings</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
